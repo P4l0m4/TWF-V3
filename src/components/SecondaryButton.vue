02b5cfdf3ad1bc844f52d7e12d728106b2cc9307
Paloma Tejeda <palomatejeda81@gmail.com>
2025-05-19 19:15:51 +0200

    replace primary and secondary buttons components by span

--- src/components/SecondaryButton.vue
+++ src/components/SecondaryButton.vue
@@ -47,8 +47,9 @@ const iconColor = computed(() => {
 });
 </script>
 <template>
-  <button
-    type="button"
+  <span
+    role="button"
+    tabindex="0"
     class="button noselect"
     :class="variant"
     :style="{ flexDirection: direction, fontSize }"
@@ -61,7 +62,7 @@ const iconColor = computed(() => {
       :size="iconSize || undefined"
       :color="iconColor"
     />
-  </button>
+  </span>
 </template>
 <style scoped lang="scss">
 .button {
